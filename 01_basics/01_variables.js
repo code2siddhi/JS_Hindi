220d9bb9e1bc2855d092eac590517ded1d86c84c
Siddhi Priya <code2siddhi@gmail.com>
2025-11-04 04:21:49 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountID = 1445533
+let accountEmail = "siddhi@google.com"
+var accountPassword = "12345"
+
+accountCity = "jaipur"
+// accountID = 2 // not allowed
+ accountEmail = "hc@hc.com"
+ accountPassword = "212121212"
+ accountCity = "Bengaluru"
+console.log(accountID);
+console.table([accountID , accountEmail , accountPassword , accountCity ]);
+
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+/*
+  Prefer not to use of var
+   because issue in block and funtional scope
+*/
+
+
+console.log(typeof(accountCity));
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,2 @@
-console.log("siddhi")
+console.log("siddhi") 
+
