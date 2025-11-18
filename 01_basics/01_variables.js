581adeae51d6ff7a5f30a81877ed63a653190149
Siddhi Priya <code2siddhi@gmail.com>
2025-11-18 09:25:21 +0000

    convesions and comapaarisons

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,14 +1,14 @@
 const accountID = 1445533
 let accountEmail = "siddhi@google.com"
 var accountPassword = "12345"
-
+let accountState;
 accountCity = "jaipur"
 // accountID = 2 // not allowed
  accountEmail = "hc@hc.com"
  accountPassword = "212121212"
  accountCity = "Bengaluru"
 console.log(accountID);
-console.table([accountID , accountEmail , accountPassword , accountCity ]);
+console.table([accountID , accountEmail , accountPassword , accountCity ,accountState ]);
 
 // console.log(accountEmail);
 // console.log(accountPassword);
--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,7 +22,68 @@
 // "" => false
 // "hitesh" => true
 
-let someNumber = 33
- let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33
+//  let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+// //****************************** Operations ********************
+
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2)
+//  console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+// let str1 = "hello"
+// let str2 = "hitesh"
+// let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+
+// console.log((3+4)*5%3)
+
+// console.log(true);
+
+// console.log(+true)
+
+// console.log(false)
+// console.log(+false)
+// console.log(+"")
+
+let num1 , num2 ,num3
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
+++gamecounter
+
+console.log(gamecounter)
+
+let x = 3
+const y = x++;
+console.log(`x:${x},y:${y}`);
+let a = 3 ;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+
+//expected output "a:4 , b:4"
+
+// link  to 
+ 
+//   https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+ 
