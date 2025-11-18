05230ba328ff3a806d61361092f4ba783375f96c
Siddhi Priya <code2siddhi@gmail.com>
2025-11-18 11:06:11 +0000

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,52 @@
+// // primitive 
+
+// //7 types : String ,  Number , Boolean , null , undefiend , Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = symbol('123')
+// console.log(id === anotherId)
+
+// // const bigNumber = 643654754674553476n
+
+
+
+
+// // Refrence (Non Primitive)
+
+// // Array , Objects , Functions 
+
+// const heros = ["shaktiman", "naagaraj", "doga"]
+// let myobj = {
+//     name: "hitesh",
+//     age: 22,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello world")
+// }
+
+// console.log(typeof bigNumber);
+
+console.log("siddhi")
+
+const score = 100
+console.log(score)
+
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+ let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+//const bigNumber =1232324343433657n
+
+//  https://262.ecma-international.org/5.1/#sec-11.4.3
